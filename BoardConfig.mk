ddb95b174c34779bd73f96d9fba0b9a5956d83d9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-03-03 13:18:16 +0100

    ysl: Kill console parameters
    Change-Id: I69a14e00cfc3291175351259f2d8cbd6b0dc827a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TARGET_INIT_VENDOR_LIB := libinit_ysl
 TARGET_RECOVERY_DEVICE_MODULES := libinit_ysl
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive audit=0
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive audit=0
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
